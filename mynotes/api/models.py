92d66e56c7437041cfa74b07a68c2cf9993a3369
tacheraSasi <tacherasasi@gmail.com>
2024-08-14 15:26:20 +0300

    added some views

--- mynotes/api/models.py
+++ mynotes/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    body = models.TextField(null=True,blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.body[0:50]
